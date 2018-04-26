b572f3959469656e22c62d1ff1603e630ec9116e
bpeter97 <brian.lee.peter@gmail.com>
2018-04-25 20:32:57 -0700

    Revert "Add some other modifications"
    This reverts commit 6e3cb73684bd64004e6e3d6ce0443c71bc88f68e.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
-  secretOrKey: 'secret'
+  mongoURI: "mongodb://peter:pass@ds015928.mlab.com:15928/esportconnections",
+  secretOrKey: "secret"
 };
