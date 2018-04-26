6e3cb73684bd64004e6e3d6ce0443c71bc88f68e
bpeter97 <brian.lee.peter@gmail.com>
2018-04-25 20:32:57 -0700

    Add some other modifications

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://peter:pass@ds015928.mlab.com:15928/esportconnections",
-  secretOrKey: "secret"
+  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  secretOrKey: 'secret'
 };
